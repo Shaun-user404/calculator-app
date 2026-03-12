c8db0c68a774cfa81d2229216a9f46df60e2546f
Shaun Moalosi <shaunmoalosi90@gmail.com>
2026-03-13 01:34:14 +0200

    Added calculator ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Calculator App",
+      style:TextStyle(
+        fontWeight: FontWeight(10),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
       ),
-      body: Center(
-
+      backgroundColor: Colors.grey,
+    ),
+    
+    body: Center(
+      child: Container(
+        width: 350,
+        color: Colors.brown[300],
         child: Column(
-
-          mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: null,
+                child: Text("Converter"),
+                ),
+                ElevatedButton(onPressed: null,
+                child: Text("Symbols"),
+                ),
+                ElevatedButton(onPressed: null,
+                child: Text("History"),
+                ),
+                
+              ],
             ),
+            const Divider(height:30, thickness:1),
+            Container(
+              height: 150,
+               margin: const EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+
+            ),
+            
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+    ),
+  ),
+));
